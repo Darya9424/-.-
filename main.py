028cacc78ca7daf9dfa532fae862e393bdc55dde
Darya9424 <dburyachenko9424@gmail.com>
2024-06-30 23:04:03 +0500

    revision main

--- main.py
+++ main.py
@@ -39,39 +39,44 @@ def get_shop_list_by_dishes(dishes, person_count):
 
 
 
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    line_1 = {}
-    count_1 = 0
-    for line in file_1.readlines():
-        count_1 += 1
-        line_1['1.txt'] = count_1
-with open('1.txt', 'r', encoding='utf-8') as file_1:
-    text_1 = file_1.read()
+with open('1.txt', encoding='utf-8') as f1:
+    text1 = f1.read()
+with open('2.txt', encoding='utf-8') as f2:
+    text2 = f2.read()
+with open('3.txt', encoding='utf-8') as f3:
+    text3 = f3.read()
 
+def count_line_length(text):
+    return len(text.split('\n'))
 
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    line_2 = {}
-    count_2 = 0
-    for line in file_2.readlines():
-        count_2 += 1
-        line_2['2.txt'] = count_2
-with open('2.txt', 'r', encoding='utf-8') as file_2:
-    text_2 = file_2.read()
+main_dict = {
+    '1':{'file_name': '1.txt', 'text': text1},
+    '2':{'file_name': '2.txt', 'text': text2},
+    '3':{'file_name': '3.txt', 'text': text3}
+            }
+lengthes_list = []
 
+for i in main_dict.values():
+    (i['length']) = count_line_length(i['text'])
+    lengthes_list.append(i['length'])
 
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    line_3 = {}
-    count_3 = 0
-    for line in file_3.readlines():
-        count_3 += 1
-        line_3['3.txt'] = count_3
-with open('3.txt', 'r', encoding='utf-8') as file_3:
-    text_3 = file_3.read()
+lengthes_list.sort()
 
-join = sorted(list(line_1.items()) + list(line_2.items()) + list(line_3.items()), key=lambda x: x[1])
+def create_text():
+    text_list = []
+    position = 0
+    for length in lengthes_list:
+        for v in main_dict.values():
+            if length == v['length']:
+                position += 1
+                text_list.append(v['file_name'])
+                text_list.append(str(position))
+                text_list.append(v['text'])
+    return '\n'.join(text_list)
 
-with open('result.txt', 'w', encoding='utf-8') as file_result:
-    for line in join:
-        file_result.write(f'{join[0][0]}\n {join[0][1]}\n {text_2}\n {join[1][0]}\n {join[1][1]}\n {text_1}\n {join[2][0]}\n'
-                          f'{join[2][1]}\n {text_3}\n')
+full_text = create_text()
+
+with open('result.txt', 'w', encoding='utf-8') as f4:
+    f4.write(full_text)
+
 
